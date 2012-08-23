88af0a1b182d9f2152f7cfef04039f09ae53d631
Hatta Yuthafiga <crbru.yuthafiga@gmail.com>
2012-08-23 18:03:54 +0700

    AIOTData
    AIOT Data Library

---
+++ AIOTData/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("AIOTData")> 
+<Assembly: AssemblyDescription("All In One Tools")> 
+<Assembly: AssemblyCompany("O2PSoftLabs")> 
+<Assembly: AssemblyProduct("AIOTData")> 
+<Assembly: AssemblyCopyright("Copyright © O2PSoftLabs  2012")> 
+<Assembly: AssemblyTrademark("O2PSoftLabs @ 2012")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("66c43022-c55b-45ea-8c0f-3d8eb49c630c")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.1.3.0")> 
+<Assembly: AssemblyFileVersion("1.1.3.0")> 
